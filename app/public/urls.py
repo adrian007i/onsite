68036d2abd2e48eb96a60a528d0ebec20c55a0cf
Adrian John <adrianjohn.developer@gmail.com>
2024-09-04 19:44:17 -0500

    login page

--- app/public/urls.py
+++ app/public/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.user_login, name="login"),
+    path("login_ajax", views.login_ajax),
     path("logout", views.user_logout, name="logout"),
     path("register", views.register, name="register"),
     path("register_ajax", views.register_ajax,),
--- app/public/views.py
+++ app/public/views.py
@@ -4,12 +4,29 @@ from django.db import IntegrityError
 from django.core.exceptions import BadRequest
 from app.models import *
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout ,get_user_model
+from django.contrib.auth.hashers import make_password
 
 def index(request):
      return HttpResponseRedirect("/jobs") 
 
 def user_login(request): 
     return render(request , "public/login.html") 
+
+def login_ajax(request):
+     
+    try:
+        print(request.POST.get("password") , request.POST.get("email"))
+        user = authenticate(request, username=request.POST.get("email"), password=request.POST.get("password"))  
+        print(user)
+        if user:
+            auth_login(request,user)
+            return JsonResponse ({'redirect' : f'/{user.role}/profile'})
+        else:
+            return JsonResponse({"server" : "Invalid Credentials"} , status=400) 
+        
+    except Exception as e:
+        return JsonResponse({"server" : "Something went wrong. Try Later!"} , status=400)
+ 
 
 def user_logout(request):
     return HttpResponse("logout")
@@ -26,8 +43,8 @@ def register_ajax(request):
         last_name = request.POST.get("last_name"), 
         headline = request.POST.get("headline"), 
         role = request.POST.get("role"),
-        password= request.POST.get("password")) 
-        
+        password= make_password(request.POST.get("password"))) 
+              
         auth_login(request,user)
  
         return JsonResponse ({'redirect' : f'/{user.role}/profile'})
