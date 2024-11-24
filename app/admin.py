117cf55abb100b108f9fbc1f4652a45a7c1e6eac
Adrian John <adrianjohn.developer@gmail.com>
2024-11-24 00:51:18 -0600

    admin portal

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-# from app.models import UserProfile
+from app.models.user import User
+from app.models.applicant import Applicant
+from app.models.saved import Saved
 # Register your models here.
 
-# admin.site.register(UserProfile)
+admin.site.register(User)
+admin.site.register(Applicant)
+admin.site.register(Saved)
+
