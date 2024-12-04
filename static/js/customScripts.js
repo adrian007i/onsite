45fdb8292d749f8fb3f664cce3a435eebf43297f
Adrian John <adrianjohn.developer@gmail.com>
2024-12-03 23:36:06 -0600

    attach and detach from profile

--- static/js/customScripts.js
+++ static/js/customScripts.js
@@ -125,13 +125,14 @@ const setPreview = (target) => {
 
     if(file.length === 0 ){ 
         $("#toggle_preview label").removeClass("d-none");
-        $("#toggle_preview div").addClass("d-none");
+        $("#toggle_preview iframe").addClass("d-none");
         $("#resume_logo label").text("Attach Resume");
     }else{
         const fileURL = URL.createObjectURL(file[0]);
-        $('#file_preview iframe').attr("src",fileURL);
+        document.getElementById('file_preview').src = fileURL;
         $("#toggle_preview label").addClass("d-none");
-        $("#toggle_preview div").removeClass("d-none");
+        $("#toggle_preview iframe").removeClass("d-none");
         $("#resume_logo label").text("Change Resume Selection");
     }
 }
+ 
